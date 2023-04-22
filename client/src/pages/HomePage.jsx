9ad1dc2cf8740a27f771686b71ff7b2e7bf0c6cd
Glen <dsouzaglen30@gmail.com>
2023-04-22 23:24:22 +0530

    rebase Homepage og repo

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { FieldContainer, PredictionModal } from "../components";
 import axios from "axios";
 import "../stylesheets/HomePage.css";
 import toast, { Toaster } from "react-hot-toast";
+import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import {
   setAge,
@@ -21,6 +22,7 @@ import {
 } from "../redux/infoSlice";
 import { backendUrl } from "../utils";
 const HomePage = () => {
+  const location = useLocation();
   const [isShow, setIsShow] = useState(false);
   const [percentage, setPercentage] = useState(0);
   const toggleModal = () => {
@@ -58,6 +60,13 @@ const HomePage = () => {
       !ca
     ) {
       toast.error("Please enter all fields");
+      return;
+    }
+
+    //check if the mobile is present in session storage if not then toast error
+    if (sessionStorage.getItem("mobile") === null) {
+      toast.error("Please login first");
+      return;
     }
 
     const formData = {
@@ -77,7 +86,10 @@ const HomePage = () => {
     };
 
     try {
-      let res = await axios.post(`${backendUrl}/predict`, formData);
+      let res = await axios.post(
+        `${backendUrl}/predict?mobile=${sessionStorage.getItem("mobile")}`,
+        formData
+      );
       console.log(res.data);
 
       if (res.data?.SUCCESS) {
@@ -237,9 +249,9 @@ const HomePage = () => {
                 val={thal}
                 func={setThal}
                 options={[
-                  { value: 1, label: "1" },
-                  { value: 2, label: "2" },
-                  { value: 3, label: "3" },
+                  { value: 1, label: "Normal" },
+                  { value: 2, label: "Fixed Defect" },
+                  { value: 3, label: "Reversible Defect" },
                 ]}
               />
               <FieldContainer
